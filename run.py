71114c2a35a6ba56f111918c24594d4cb121f544
Fereshte Mozafari <mozafari@sysadmins-MacBook-Pro.local>
2019-12-07 08:56:34 +0100

    data_preprocessing/read_and_preprocess.py

--- run.py
+++ run.py
@@ -1,6 +1,48 @@
+from numpy.random import seed
+seed(1)
+import tensorflow
+tensorflow.random.set_seed(2)
+
 from data_preprocessing.read_and_preprocess import *
+from sklearn.model_selection import train_test_split
+from models.LSTM import LSTM_
+
+full = True
+glove_dimension = 25
+max_words = 40
 
 # load data
 print("Loading Data...")
-train_pos, train_neg, test, glove_embedings = load_data()
+X_train, Y_train, X_test, embeding_matrix = load_data(FULL=full , GLOVE_DIMENSION=glove_dimension , MAX_WORDS=max_words)
 print("Data Loaded...")
+
+print(embeding_matrix.shape[0])
+
+xtrain , xvalid, ytrain, yvalid = train_test_split(X_train, Y_train, shuffle=True, test_size=0.1, random_state=0) # for cross validation
+
+# model parameters
+params = {
+    'loss': 'binary_crossentropy',
+    'num_neurons': 100,
+    'dropout': 0.0,
+    'batch_size': 512,
+    'recurrent_dropout': 0.0,
+    'epochs': 5,
+    'dense_activation': 'sigmoid',
+    'optimizer': 'RMSprop'
+}
+
+model_name = "LSTM"
+lstm = LSTM_(model_name , embeding_matrix , max_words)
+lstm.build_model(params)
+print("----model build----- ")
+print("----model summary----")
+print(lstm.model.summary())
+# Train the model ---> save the weights with best validation loss
+print("----training.....")
+lstm.train(xtrain, ytrain, epochs=params["epochs"], batch_size=params["batch_size"], validation_data=(xvalid, yvalid))
+print("------model trained------")
+print('_________________________________')
+print(model_name)
+print('_________________________________')
+
